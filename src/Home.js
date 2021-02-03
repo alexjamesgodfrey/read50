f4fb3c34664180368f047fbd53091cff25b63576
Alex Godfrey <alexjamesgodfrey@gmail.com>
2021-02-03 15:50:51 -0500

    d

--- src/Home.js
+++ src/Home.js
@@ -58,7 +58,7 @@ class Home extends Component {
     }
 
     getNumbers = async () => {
-        const getUsers = await fetch('http://localhost:5000/api/usercount');
+        const getUsers = await fetch('https://read50.herokuapp.com/api/usercount');
         const userJson = await getUsers.json();
         
         const getBooks = await fetch('/api/3numsum/bookcount');
