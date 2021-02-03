81fe93a4d6e581e4a5edf9fa3e2a420565bc9a37
Alex Godfrey <alexjamesgodfrey@gmail.com>
2021-02-03 14:48:47 -0500

    deleted package-lock.json

--- server/index.js
+++ server/index.js
@@ -9,8 +9,6 @@ const PORT = process.env.PORT || 5000;
 //process.env.PORT
 //process.env.NODE_ENV => production or undefined
 
-app.use(express.static(path.join(__dirname, "../build")));
-
 //middleware
 app.use(cors());
 app.use(express.json()); //req.body
