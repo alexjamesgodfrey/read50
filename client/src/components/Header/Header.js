5ace4be2631b422ff8a55e1ac3334e4894095e7c
Alex Godfrey <alexjamesgodfrey@gmail.com>
2021-04-15 15:20:58 -0400

    commented logged in dropdown

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,18 +1,14 @@
-import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import BetaAlert from './BetaAlert.js';
 import CustomNav from './CustomNav.js';
-import './Header.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header = () => {
-
   return (
     <header>
       <BetaAlert />
       <CustomNav />
     </header>
   )
-  
 }
 
 export default Header;
--- client/src/components/Header/LoggedInDropdown.js
+++ client/src/components/Header/LoggedInDropdown.js
@@ -1,21 +1,23 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useCookies  } from 'react-cookie';
 import { useAuth0 } from '@auth0/auth0-react';
 import { NavDropdown } from 'react-bootstrap'
 import './Header.scss';
 
 const LoggedInDropdown = () => {
-    const { user, logout } = useAuth0();
-    const [cookies, setCookie] = useCookies(['auth0', 'username']);
+  //auth0 user and logout
+  const { user, logout } = useAuth0();
+  //auth0 sub cookie and username cookie
+  const [cookies, setCookie] = useCookies(['auth0', 'username']);
 
-    const setAuth = async () => {
-      setCookie('auth0', user.sub, { path: '/', maxAge: 3600 });
-      setCookie('username', user['https://www.read50.com/username'], { path: '/', maxAge: 3600 });
-      console.log('auth0 cookie set to ' + cookies.auth0);
-      console.log('username cookie set to ' + cookies.username);
-    }
+  //sets auth0 and username cookie (age three hours)
+  const setAuth = async () => {
+    setCookie('auth0', user.sub, { path: '/', maxAge: 10800 });
+    setCookie('username', user['https://www.read50.com/username'], { path: '/', maxAge: 10800 });
+  }
 
-    const addToDB = async (sub, email, username, picture, logcount) => {
+  //creates user JSON and adds to database
+  const addToDB = async (sub, email, username, picture, logcount) => {
     try {
       const auth0User = `
       {"auth0_id": "${sub}",
@@ -35,31 +37,30 @@ const LoggedInDropdown = () => {
     }
   };
 
-  //function sets this.state.inDB to true, if user is in postgresDB, else adds and sets to true
-  //params: user's auth0 id
+  //checks if the user is in the database. if they are not, they are added.
   const checkIfDB = async (sub, email, username, picture, logcount) => {
     //responseText = valid user if user is in database
     const response = await fetch(`/api/users/herecheck/${sub}`);
     const responseText = await response.text();
     if (responseText !== 'valid user') {
       //add to database function and set inDB to true
-        addToDB(sub, email, username, picture, logcount);
-        console.log('added to db');
+      addToDB(sub, email, username, picture, logcount);
     }
   };
 
-    useEffect(() => {
-        setAuth();
-        checkIfDB(user.sub, user.email, user['https://www.read50.com/username'], user.picture, user['https://www.read50.com/logincount']);
-    }, [])
+  //sets cookies and database on each run
+  useEffect(() => {
+      setAuth();
+      checkIfDB(user.sub, user.email, user['https://www.read50.com/username'], user.picture, user['https://www.read50.com/logincount']);
+  }, [])
 
-    return (
-        <NavDropdown className="mr-auto" title='myread50'>
-            <NavDropdown.Item id="dropdown-link" href="#profile">profile</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item id="dropdown-link" onClick={() => logout()}>sign out</NavDropdown.Item>
-        </NavDropdown>
-    );
+  return (
+    <NavDropdown className="mr-auto" title='myread50'>
+        <NavDropdown.Item id="dropdown-link" href="#profile">profile</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item id="dropdown-link" onClick={() => logout()}>sign out</NavDropdown.Item>
+    </NavDropdown>
+  );
 }
 
 export default LoggedInDropdown;
